3c4c90609147bf951cca1c80a37db18a5a2901db
Cherry Rose Semeña <cherryrosesemena1102@gmail.com>
2018-02-06 02:36:04 +0100

    last commit, I'm so tired and sleepy..babush!

--- KeyValue_python/dbms.py
+++ KeyValue_python/dbms.py
@@ -1,27 +1,76 @@
+import random
+import string
+
+indexMap = {}
+db = 'database.txt'
+hashMapIndex = 'hashMapIndex'
 
 def read():
-    f = open('database.txt','r')
-    message = f.read()
-    lines = message.split('\n')
+    f = open(db,'r')
+    text = f.read()
+    lines =  text.split('\n')
+    print('READING THIS')
+    # print(lines)
     for el in lines:
-        msg = dec(el)
-        print(msg)
+        element = dec(el)
+        print(element)
+        arr = element.split(':')
+        if len(arr) == 2:
+            key = arr[0]
+            val = arr[1]
+            indexMap[key] = val;
     f.close()
 
-def writeAppend():
-    f = open('database.txt','a')
-    f.write(enc('hello') + '\n')
-    f.write(enc('world') + '\n')
+def write(key,val):
+    pair = "%s:%s"%(key,val)
+    f = open(db,'a')
+    print(pair)
+    f.write(enc(pair) + '\n')
+    addNewIndex(key,val)
+    createHashMapIndex(key,val)
     f.close()
 
-def dec(str):
-     return ''.join(chr(int(str[i*8:i*8+8],2)) for i in range(len(str)//7))
+def populateDB():
+    write('dog','Bulder')
+    write('cat','Muning')
+    for el in range(10):
+      write(el, getRandom())
+    print('Done populating DB')
 
-def enc(str):
-	return ' '.join(format(ord(x), 'b') for x in str)
+def addNewIndex(key,val):
+    read()
+    # if doesn't exist, add new index and write append to the db file
+    # else replace/update the key value
+    indexMap[key] = val;
 
 
-# writeBinaryFile()
-# readBinaryFile()
-writeAppend()
-read()
+def createHashMapIndex(key, val):
+    pair = "%s:%s"%(key,val)
+    f = open(hashMapIndex,'a')
+    print('myIndexMap')
+    print(indexMap)
+    for key in indexMap.keys():
+        value = indexMap[key]
+        print(value)
+    f.write(pair + '\n')
+    f.close()
+
+def dec(binaryString):
+    # print('DECODING...' + binaryString)
+    return "".join([chr(int(binaryString[i:i+8],2)) for i in range(0,len(binaryString),8)])
+
+
+def enc(string):
+    # print('ENCODING...' + string)
+    return "".join([format(ord(char),'#010b')[2:] for char in string])
+
+
+def getRandom():
+    char = string.ascii_letters
+    return ''.join(random.sample(char, 5))
+
+# MAIN
+print('WRITING THIS')
+populateDB()
+# print('---------------------------------')
+# read()
